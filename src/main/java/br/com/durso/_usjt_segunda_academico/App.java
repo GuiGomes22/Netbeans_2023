ed290d9f5d7316435b75105f1795ccc45ce62aea
Guilherme Gomes <gui-h-gomes@hotmail.com>
2023-10-02 21:16:44 -0300

    apll(login): campos login e senha

---
+++ src/main/java/br/com/durso/_usjt_segunda_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.durso._usjt_segunda_academico;
+
+/**
+ *
+ * @author 823220260
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
